05db28f07605cdc570afaa39fcd3074da88e9b46
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-02-02 17:39:11 -0500

    Fix error message when passwords do not match

--- src/app/routes/sign-up/sign-up.component.ts
+++ src/app/routes/sign-up/sign-up.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, AbstractControl, FormGroupDirective, NgForm, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 
+export class PasswordChecker implements ErrorStateMatcher {
+  isErrorState(control: AbstractControl<any, any> | null, form: FormGroupDirective | NgForm | null): boolean {
+    return control?.parent?.errors?.['passwordMismatch'];
+  }
+}
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -10,6 +16,7 @@ import { ErrorStateMatcher } from '@angular/material/core';
 export class SignUpComponent {
 
   signUpForm: FormGroup;
+  passwordChecker = new PasswordChecker();
 
   constructor() {
     this.signUpForm = new FormGroup({
@@ -17,7 +24,13 @@ export class SignUpComponent {
       lastName: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
-      reenteredPass: new FormControl('', [Validators.required]),
-    });
+      reenteredPass: new FormControl(''),
+    }, { validators: this.checkPasswords });
+  }
+
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    const password = group.get('password')?.value;
+    const reenteredPass = group.get('reenteredPass')?.value;
+    return password == reenteredPass ? null : { passwordMismatch: true };
   }
 }
