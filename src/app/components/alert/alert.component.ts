eeac1fe8044a2056f50ad51cec17d0d8d6d6996f
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-02-05 12:46:02 -0500

    Add custom styles to alert component

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -8,5 +8,25 @@ import { Component, Input } from '@angular/core';
 export class AlertComponent {
 
   @Input()
+  type?: "success" | "info" | "error"
+  @Input()
   message?: string;
+
+  alertClassName(): string {
+    if (this.type)
+      return `alert alert--${this.type}`;
+    else
+      return 'alert';
+  }
+
+  fontIcon(): string {
+    if (this.type == "success")
+      return "task_alt";
+    else if (this.type == "info")
+      return "info_outline";
+    else if (this.type == "error")
+      return "error_outline";
+    else
+      return "error_outline";
+  }
 }
