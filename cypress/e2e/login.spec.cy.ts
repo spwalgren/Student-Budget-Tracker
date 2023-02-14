5d62bcfebb8dcd68480b9bd4ef75c8c44da92d14
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-02-14 11:59:41 -0500

    fully working cypress test 1

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -2,6 +2,9 @@ describe('login', () => {
   it('Should not login if the form is invalid', () => {
     cy.visit('/')
     cy.get('button').eq(0).click()
+    cy.get('[formControlName="email"]').type('profanis@gmail.com');
     cy.url().should('includes', 'login');
+    cy.get('button').eq(0).click()
+    cy.url().should('not.include', 'dashboard');
   })
 })
