6559c27395c99baaf54b729ec3142dd08e5ad45e
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-02-18 12:12:08 -0500

    working sign up test added...need to clean code

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -28,23 +28,35 @@ describe('login', () => {
     cy.url().should('include', 'sign-up');
   })
 
-  // it('Should let user sign up... clicking \"Sign Up\" from login page', () => {
+  it('Should let user sign up... clicking \"Sign Up\" from login page', () => {
+    cy.visit('/')
+    cy.get('[data-cy="Log In"]').click()
+    cy.url().should('includes', 'login');
+    cy.get('[data-cy="Sign-Up"]').click({waitForAnimations: false})
+    cy.url().should('include', 'sign-up');
+    cy.get('[data-cy="testFirstName"]').type('Bobby');
+    cy.get('[data-cy="testLastName"]').type('Fergison');
+    cy.get('[formControlName="email"]').type('bobby@gmail.com');
+    cy.get('[formControlName="password"]').type('bobbyIsTheBest');
+    cy.get('[formControlName="reenteredPass"]').type('bobbyIsTheBest');
+    cy.url().should('includes', 'sign-up');
+    cy.get('[data-cy="Submit Sign-Up"]', { timeout: 16000 }).click();
+    cy.url().should('include', 'login');
+
+  })
+  
+  // it('Should let user sign up... clicking \"Sign Up\" from dashboard', () => {
   //   cy.visit('/')
-  //   cy.get('[data-cy="Log In"]').click()
-  //   cy.get('[formControlName="email"]').type('bob@gmail.com');
-  //   cy.get('[formControlName="password"]').type('b0btheb3st');
-  //   cy.url().should('includes', 'login');
-  //   cy.get('[data-cy="Sign-Up"]').click()
+  //   cy.get('[data-cy="Sign Up"]').click({force:true})
   //   cy.url().should('include', 'sign-up');
-  //   cy.get('[formControlName="firstName"]').type('Bobby');
+  //   cy.get('[formControlName="firstName"]').type('Bobby', timeout: 20);
   //   cy.get('[formControlName="lastName"]').type('Fergison');
   //   cy.get('[formControlName="email"]').type('bobby@gmail.com');
   //   cy.get('[formControlName="password"]').type('bobbyIsTheBest');
   //   cy.get('[formControlName="reenteredPass"]').type('bobbyIsTheBest');
   //   cy.url().should('includes', 'sign-up');
-  //   cy.get('[data-cy="Submit Sign-Up"]').click();
+  //   cy.get('[data-cy="Submit Sign-Up"]').click({force:true});
   //   cy.url().should('include', 'dashboard');
-
   // })
 
   //test uncompleted sign up
