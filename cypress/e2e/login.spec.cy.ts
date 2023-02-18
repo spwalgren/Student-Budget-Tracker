85a91b319b7843e5aa207605fffcf32629b0bf31
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-02-18 10:58:16 -0500

    added attributes to buttons using best practices for testing

--- cypress/e2e/login.spec.cy.ts
+++ cypress/e2e/login.spec.cy.ts
@@ -1,10 +1,10 @@
 describe('login', () => {
   it('Should not login if the form is invalid', () => {
     cy.visit('/')
-    cy.get('button').eq(0).click()
-    cy.get('[formControlName="email"]').type('profanis@gmail.com');
+    cy.get('[data-cy="Log In"]').click()
+    cy.get('[formControlName="email"]').type('bob@gmail.com');
     cy.url().should('includes', 'login');
-    cy.get('button').eq(0).click()
+    cy.get('[data-cy="Submit Login"]').click()
     cy.url().should('not.include', 'dashboard');
   })
 })
