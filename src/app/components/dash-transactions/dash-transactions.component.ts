58b2fe0b4b81dea7a936d10c4b7d7a2cd8c20e4a
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-02-23 14:17:15 -0500

    new transaction -save button works

--- src/app/components/dash-transactions/dash-transactions.component.ts
+++ src/app/components/dash-transactions/dash-transactions.component.ts
@@ -53,6 +53,7 @@ export class DashTransactionsComponent {
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
+      console.log(result);
     });
   }
 
--- src/app/components/transactions-modal/transactions-modal.component.ts
+++ src/app/components/transactions-modal/transactions-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
 
 @Component({
   selector: 'app-transactions-modal',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./transactions-modal.component.css']
 })
 export class TransactionsModalComponent {
+  constructor(public dialogRef: MatDialogRef<TransactionsModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
 
+    } //data b/w data and source
+
+    ngOnInit(){
+
+    }
+
+    save(){
+      this.dialogRef.close("DATA SAVED");
+      //save doc
+    }
 }
