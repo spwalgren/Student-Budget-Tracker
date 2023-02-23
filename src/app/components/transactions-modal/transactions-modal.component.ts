a5bdd05ad29ddcffc13d215572a882c35e78615c
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-02-23 15:02:16 -0500

    some infrastructure built to store new transactions data as Transaciton obj

--- src/app/components/transactions-modal/transactions-modal.component.ts
+++ src/app/components/transactions-modal/transactions-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
+import { Transaction } from 'src/types/transaction-system';
 
 @Component({
   selector: 'app-transactions-modal',
@@ -7,17 +8,25 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
   styleUrls: ['./transactions-modal.component.css']
 })
 export class TransactionsModalComponent {
+
+  transaction: Transaction = {
+    userId: 0,
+    name: '',
+    amount: 0,
+    date: '',
+    category: '',
+    description: ''
+  };
+
   constructor(public dialogRef: MatDialogRef<TransactionsModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) { 
 
     } //data b/w data and source
 
-    ngOnInit(){
-
-    }
+    ngOnInit(){}
 
     save(){
-      this.dialogRef.close("DATA SAVED");
+      this.dialogRef.close(this.transaction);
       //save doc
     }
 }
