824906baf119aa5ff00bade201b73ab1abcbcf66
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-02-26 19:11:31 -0500

    Create cypress test for alert component

---
+++ src/app/components/alert/alert.component.cy.ts
@@ -0,0 +1,51 @@
+import { MatIconModule } from "@angular/material/icon";
+import { AlertComponent } from "./alert.component";
+
+describe('AlertComponent', () => {
+
+  it('should mount', () => {
+    cy.mount(AlertComponent);
+  });
+
+  it('should display text', () => {
+    cy.mount(AlertComponent, {
+      componentProperties: {
+        message: "This is an error"
+      }
+    });
+    cy.get('div').contains('This is an error');
+  });
+
+  it('should display different colors', () => {
+    cy.mount(AlertComponent, {
+      componentProperties: {
+        type: "success"
+      }
+    });
+    cy.get('div.alert').should('have.css', 'background-color', 'rgb(237, 247, 237)').should('have.css', 'color', 'rgb(28, 68, 30)');
+    cy.mount(AlertComponent, {
+      componentProperties: {
+        type: "info"
+      }
+    });
+    cy.get('div.alert').should('have.css', 'background-color', 'rgb(229, 246, 253)').should('have.css', 'color', 'rgb(1, 67, 97)');
+    cy.mount(AlertComponent, {
+      componentProperties: {
+        type: "error"
+      }
+    });
+    cy.get('div.alert').should('have.css', 'background-color', 'rgb(253, 237, 237)').should('have.css', 'color', 'rgb(95, 33, 32)');
+    cy.mount(AlertComponent);
+    cy.get('div.alert').should('have.css', 'background-color', 'rgb(253, 237, 237)').should('have.css', 'color', 'rgb(95, 33, 32)');
+  });
+
+  it('should display an icon', () => {
+    cy.mount(AlertComponent, {
+      componentProperties: {
+        type: "success"
+      },
+      imports: [MatIconModule]
+    });
+    cy.get('mat-icon').should('exist');
+  })
+});
