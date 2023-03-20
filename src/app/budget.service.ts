672d6f8da652b75752fd65140068f9234abe6dbf
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-03-20 11:31:40 -0400

    Add CRUD funcs to budget service

--- src/app/budget.service.ts
+++ src/app/budget.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, of, map, from } from 'rxjs';
-import { getBudgets } from 'src/sample-budget-data';
-import { GetBudgetsResponse } from 'src/types/budget-system';
+import { createBudget, deleteBudget, getBudgets, updateBudget } from 'src/sample-budget-data';
+import { GenericResponse } from 'src/types/api-system';
+import { CreateBudgetRequest, CreateBudgetResponse, GetBudgetsResponse, UpdateBudgetRequest } from 'src/types/budget-system';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,19 @@ export class BudgetService {
 
   constructor(private http: HttpClient) { }
 
+  createBudget(requestData: CreateBudgetRequest): Observable<CreateBudgetResponse> {
+    return from(createBudget(requestData));
+  }
+
   getBudgets(): Observable<GetBudgetsResponse> {
     return from(getBudgets());
+  }
+
+  updateBudget(requestData: UpdateBudgetRequest): Observable<GenericResponse> {
+    return from(updateBudget(requestData));
+  }
+
+  deleteBudget(budgetId: number): Observable<GenericResponse> {
+    return from(deleteBudget(budgetId));
   }
 }
