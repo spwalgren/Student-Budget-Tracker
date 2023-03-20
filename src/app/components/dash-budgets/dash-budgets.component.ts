dc4a80e08ac107ee016cd06d39994df792a47b7f
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-03-20 12:15:58 -0400

    Add dialog to budgets page

--- src/app/components/dash-budgets/dash-budgets.component.ts
+++ src/app/components/dash-budgets/dash-budgets.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { Budget } from 'src/types/budget-system';
 import { BudgetService } from 'src/app/budget.service';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+
+export interface BudgetsDialogData {
+  mode: "add" | "edit",
+  data: Budget
+}
 
 @Component({
   selector: 'app-dash-budgets',
@@ -12,7 +18,10 @@ export class DashBudgetsComponent {
   budgetData: Budget[] = []
   existingCategories: string[] = []
 
-  constructor(private budgetService: BudgetService) { }
+  constructor(
+    private budgetService: BudgetService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.budgetService.getBudgets()
@@ -30,5 +39,30 @@ export class DashBudgetsComponent {
 
   getFilteredData(category: string, budgetData: Budget[]) {
     return budgetData.filter((elem) => elem.data.category === category);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(BudgetsDialog, {
+      data: { mode: "add", data: null }
+    });
+
+    dialogRef.afterClosed().subscribe((res) => {
+      console.log(res);
+    });
+  }
+}
+
+@Component({
+  selector: 'budgets-dialog',
+  templateUrl: 'budgets-dialog.html'
+})
+export class BudgetsDialog {
+  constructor(
+    public dialogRef: MatDialogRef<BudgetsDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: BudgetsDialogData
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
