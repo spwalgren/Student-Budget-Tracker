4ed1c91df0169da1f9ad2531ae4446698fc7c409
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-04-11 13:41:49 -0400

    Add styles to event card

--- src/app/components/event-card/event-card.component.ts
+++ src/app/components/event-card/event-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Period } from 'src/types/budget-system';
 import { EventContent } from 'src/types/calendar-system';
 
 @Component({
@@ -9,6 +10,39 @@ import { EventContent } from 'src/types/calendar-system';
 export class EventCardComponent {
 
   @Input()
-  eventData?: EventContent;
+  eventData: EventContent = {
+    "frequency": Period.weekly,
+    "startDate": "2023-04-11T04:00:00Z",
+    "endDate": "2023-04-18T03:59:59Z",
+    "category": "General",
+    "totalSpent": 120,
+    "amountLimit": 100
+  };
+  numberFormatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+  statusText: string;
+
+  constructor() {
+    if (this.eventData.startDate > new Date().toISOString()) {
+      this.statusText = "Upcoming";
+    } else if (this.eventData.totalSpent > this.eventData.amountLimit) {
+      this.statusText = "Overspent";
+    } else {
+      this.statusText = "On Track";
+    }
+  }
+
+  getDueDateString(): string {
+    return new Date(this.eventData.endDate).toLocaleDateString();
+  }
+
+  getDaysLeft(): number {
+    const today = new Date();
+    const endDate = new Date(this.eventData.endDate);
+    const timeDiff = endDate.getTime() - today.getTime();
+    return Math.floor(timeDiff / (1000 * 3600 * 24));
+  }
 
 }
