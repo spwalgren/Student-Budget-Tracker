fc8174249fe94a1876c915b8828912c8fd5f5ea3
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-04-11 10:51:44 -0400

    Add types for calendar-system

---
+++ src/types/calendar-system.ts
@@ -0,0 +1,20 @@
+import { Period } from "./budget-system"
+
+export interface EventContent {
+  frequency: Period,
+  startDate: string,
+  endDate: string,
+  category: string,
+  totalSpent: number,
+  amountLimit: number,
+}
+
+export interface Event {
+  userId: number,
+  eventId: number,
+  data: EventContent
+}
+
+export interface GetEventsResponse {
+  events: Event[]
+}
