6e4b07b063d144353b91015c3cf84ecb7e87ae71
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-04-13 14:07:33 -0400

    dash-progress.component.ts complete but not tested

--- src/app/components/dash-progress/dash-progress.component.ts
+++ src/app/components/dash-progress/dash-progress.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Progress } from 'src/types/progress.system';
+import { ProgressService } from 'src/app/progress.service';
+import { Period } from 'src/types/budget-system';
+import { Observable, catchError, of, map, from } from 'rxjs';
+import { Injectable } from '@angular/core';
+
 
 
 @Component({
@@ -10,15 +16,33 @@ import { MatTabsModule } from '@angular/material/tabs';
   styleUrls: ['./dash-progress.component.css']
 })
 export class DashProgressComponent {
-tab: any;
-// weeklyProgress: ProgressByPeriod = {
-//   UserID: 1,
-//   TotalSpent: 550,
-//   TransactionIDList: [1, 2, 3],
-//   BudgetIDList: [1, 2, 3],
-//   Category: 'Groceries',
-//   BudgetGoal: 1000,
-//   Frequency: 'weekly'
-// };
+  tab: any;
+  progress: Progress[] = [];
+  freq: [Period.weekly, Period.monthly, Period.yearly] | undefined;
+  weeklyTotalSpent: number = 0;
+  monthlyTotalSpent: number = 0;
+  yearlyTotalSpent: number = 0;
 
+  constructor( private progressService: ProgressService) { };
+
+  ngOnInit() {
+      this.progressService.GetProgress().subscribe((progress) => {
+        //this.progress = data;
+        if (!progress.err){
+          progress.data.forEach(elem => {
+            if ( elem.frequency == Period.weekly){
+              this.weeklyTotalSpent += elem.totalSpent;
+            }
+            if ( elem.frequency == Period.monthly){
+              this.monthlyTotalSpent += elem.totalSpent;
+            }
+            if ( elem.frequency == Period.yearly){
+              this.yearlyTotalSpent += elem.totalSpent;
+            }
+        
+          });
+          }
+      });
+    }
+  
 }
